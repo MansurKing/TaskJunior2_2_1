e45045a0ae885a1fc93b1ba238c957eb3a2c40ec
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-18 13:42:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskJunior2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "exit";
+            string userInput = "start";
+
+            Console.WriteLine("Приветствую тебя супер секретный агент, введи пароль : ");
+
+            while (userInput != password)
+            {
+            userInput = Console.ReadLine();
+
+                    Console.WriteLine("Вы ввели неправельный пароль, попробуйте еще раз!\n"); 
+            }  
+            Console.WriteLine("Урааа!!!, вот ваши самые секретные данные)))");
+        }
+    }
+}
